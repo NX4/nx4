e5f5cf00dbfef92f5ceba02f10dafd95e965fe4f
carcruz <cruz5114@gmail.com>
2018-09-14 01:07:14 -0600

    refactor: optimize server perfom

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
 const log = require('log-util');
-
-const app = express();
-
-app.use('/api', require('./remotes'));
-
-app.use(express.static(path.join(__dirname, 'client/built')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/built/index.html'));
-});
+const throng = require('throng');
 
 const port = process.env.PORT || 8000;
-app.listen(port);
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-log.debug(`Server running on port: ${port}`);
+throng({
+    workers: WORKERS,
+    lifetime: Infinity
+  },
+  start
+);
+
+function start() {
+  const app = express();
+
+  app.use('/api', require('./remotes'));
+
+  app.use(express.static(path.join(__dirname, 'client/built')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/built/index.html'));
+  });
+
+  app.listen(port);
+
+  log.debug(`Server running on port: ${port}`);
+}
