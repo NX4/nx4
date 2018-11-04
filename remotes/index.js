e237ee7bc49789e634dfdceaa8b6c84b46669f47
carcruz <cruz5114@gmail.com>
2018-11-04 17:55:33 -0600

    fix npm lib issue

--- remotes/index.js
+++ remotes/index.js
@@ -3,7 +3,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/health', require('./health'));
-router.use('/upload', require('./upload'));
-router.use('/entropy', require('./entropy'));
 
 module.exports = router;
